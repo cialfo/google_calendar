9254c74d28ed089da17f33c232fa72e1b6a93601
anirudh <anirudh.kkw@gmail.com>
2015-08-19 14:01:27 +0530

    updated constants

--- lib/google_calendar.rb
+++ lib/google_calendar.rb
@@ -3,7 +3,9 @@ module Google
   require 'google/calendar'
   require 'google/calendar_list'
   require 'google/calendar_list_entry'
+  require 'google/calendars'
   require 'google/connection'
   require 'google/event'
   require 'google/freebusy'
+  require 'google/user'
 end
